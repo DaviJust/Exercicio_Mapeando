5bca9f4fbf03dc437e059d78569ee2ad3a52edd0
Davi <davijustinocouto@gmail.com>
2022-12-17 16:15:46 -0300

    Exercicio feito link: https://stackoverflow.com/questions/74834405/map-filter-showing-people-that-have-18-years-and-abbreviate-names/74834818#74834818

---
+++ abrevNomes.js
@@ -0,0 +1,12 @@
+const catLovers = [
+    {name: "Raul", age: 27,},
+    {name: "Jose", age: 14,},
+    {name: "Maria", age: 52,},
+    {name: "Jesus", age: 17,},
+    {name: "Neo", age: 2}
+  ]
+
+  const ofAgeCatLovers = catLovers.filter(({age}) => age > 18)
+  const shortNamess = ofAgeCatLovers.map(({name}) => name.substring(0,3))
+
+  console.log(shortNamess)
